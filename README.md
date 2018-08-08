be7a5da10e32c13c9506bf6874e3d2914004a3ba
James Gaehring <gaehj457@gmail.com>
2018-08-08 13:00:26 -0400

    adding readme comments to index.js for clarity

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ This sandbox is for testing the logFactory used in the farmOS native
 and client libraries. It can be viewed here:
 https://codesandbox.io/s/github/jgaehring/farmos-logfactory
 
-You can run some tests in the console from `index.js`, or run the 
+You can run some tests in the console from `src/index.js`, or run the 
 example log statements already contained there.
 
-The main function export in `logFactory.js` is essentially the same 
+To see what's happening internally, checkout `src/logFactory.js`. 
+The main function export in there is essentially the same one that's
 contained in the farmos-native repository, though it may be outdated 
 depending on when you're reading this and how frequently I've managed 
 to update this sandbox.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+/*
+  This sandbox is for testing the logFactory used in the farmOS native
+  and client libraries.
+
+  You can run some tests in the console from `src/index.js`, or run the
+  example log statements already contained there.
+
+  To see what's happening internally, checkout `src/logFactory.js`.
+  The main function export in there is essentially the same one that's
+  contained in the farmos-native repository, though it may be outdated
+  depending on when you're reading this and how frequently I've managed
+  to update this sandbox.
+*/
+// Note that we're importing all the parameters we need along with the logFactory itself
 import logFactory, { SQL, SERVER, STORE } from './logFactory';
 
 // When no parameter is given, a default log will be created.
-console.log('Create the default log: ', logFactory());
+const defaultLog = logFactory();
+console.log('Create the default log: ', defaultLog);
 
 // Using the store parameter with an empty object will return the same result as above.
-console.log('Create log for the Vuex store: ', logFactory({}, STORE));
+const storeLog = logFactory({}, STORE);
+console.log('Create log for the Vuex store: ', storeLog);
 
 /*
   Passing an object with 2 properties and the SQL parameter will result in a log
@@ -12,20 +28,20 @@ console.log('Create log for the Vuex store: ', logFactory({}, STORE));
   removing `local_id` from the parameter object, and note that no default is set,
   so that WebSQL can generate a new local id. 
 */
-console.log(
-  'Create log for SQL insertion: ',
-  logFactory({ notes: 'farmers are cool', local_id: 123 }, SQL),
-);
+const sqlLog = logFactory({ notes: 'farmers are cool', local_id: 123 }, SQL);
+console.log('Create log for SQL insertion: ', sqlLog);
 
 /*
   Passing an object with the server parameter to send to farmOS. In this case,
   no default is set when the `id` property isn't set. Properties which the server
   will not recognize, like `isCachedLocally`, will also be ignored.
 */
-console.log(
-  'Create log for posting to farmOS server: ',
-  logFactory(
-    { notes: 'farmers are cool', local_id: 123, isCachedLocally: true },
-    SERVER,
-  ),
+const serverLog = logFactory(
+  {
+    notes: 'farmers are cool',
+    local_id: 123,
+    isCachedLocally: true,
+  },
+  SERVER,
 );
+console.log('Create log for posting to farmOS server: ', serverLog);
